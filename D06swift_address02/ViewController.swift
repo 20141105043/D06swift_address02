e517bf09f86b80a700032cfbf468bbe3035272b5
20141105043 <670140530@qq.com>
2016-07-04 08:26:30 +0800

    Initial Commit

---
+++ D06swift_address02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D06swift_address02
+//
+//  Created by 20141105043dj on 16/7/4.
+//  Copyright © 2016年 20141105043dj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
